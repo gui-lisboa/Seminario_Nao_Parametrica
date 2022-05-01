baf34d5efd9f3f10cc898d81c21b4ae5a6e234fb
Guilherme <g.lisboa.oliveira@outlook.com>
2022-05-01 17:23:44 -0300

    adiciona dataset e rascunho do exemplo de classificação

---
+++ src/utils/__init__.py
@@ -0,0 +1,1 @@
+from .carrega_dataset import *
---
+++ src/utils/carrega_dataset.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+#https://www.kaggle.com/datasets/mdmahmudulhasansuzan/students-adaptability-level-in-online-education
+CAMINHO_CSV = './datasets/students_adaptability_level_online_education.csv'
+
+def carrega_dataset():
+    dataset = pd.read_csv(CAMINHO_CSV)
+    dataset = dataset.convert_dtypes()
+    return dataset
